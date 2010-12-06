aa93c43d674f12f4c910151f245fb498e67d8880
Hiroshige Umino <hiroshige88@gmail.com>
2010-12-06 23:26:11 +0900

    toriaezu

--- anything-with-everything.el
+++ anything-with-everything.el
@@ -19,66 +19,100 @@
 ;; you can execute them like M-x `anything-hotentry'
 ;; If you want to remove candidates cache, do M-x `anything-hotentry-clear-cache'
 
-(require 'cl)
+(eval-when-compile (require 'cl))
 
 
 (defvar awe:scripts-root-dir
   "~/.emacs.d/anything/scripts/"
   "The root directory containing scripts for anything plugins")
 
-(defmacro* add-anything-plugin (function-name script &optional description source-name action-name)
-  (let ((function-symbol (intern function-name))
-        (function-description
-         (if description description
-           (concat "Execute Anything script: " script)))
-        (sname (if source-name source-name (concat "Source from " script)))
-        (aname (if action-name action-name "Open"))
-        (cache-clear-function-symbol
-         (intern (concat function-name "-clear-cache")))
-        (cache-clear-function-description
-         (format "Function to clear candidate cache for %s" script)
-         )
-                (action-symbol (intern (concat "awe:action-for-" script)))
-        (source-symbol (intern (concat "awe:source-for-" script)))
-        (script-path (concat awe:scripts-root-dir script)))
+(defmacro* add-anything-plugin (function-name script &optional description sources action-name)
+  (let* ((function-symbol (intern function-name))
+         (function-description
+          (if description description
+            (concat "Execute Anything script: " script)))
+         ;; (sname (if source-name source-name (concat "Source from " script)))
+         (aname (if action-name action-name "Open"))
+         (cache-clear-function-symbol
+          (intern (concat function-name "-clear-cache")))
+         (cache-clear-function-description
+          (format "Function to clear candidate cache for %s" script)
+          )
+         (action-symbol (intern (concat "awe:action-for-" script)))
+         ;;(sources (if (stringp sources) '(("list" . sources)) sources))
+         ;;(source-symbol (intern (concat "awe:source-for-" script)))
+         (source-symbols (mapcar (lambda (pair)
+                                   ;; XXX
+                                   (message
+                                    (format "-----awe:source-for-%s-in-%s"
+                                            (car pair)
+                                            script))
+                                   ;; XXX end
+                                   (intern
+                                    (format "awe:source-for-%s-in-%s"
+                                            (car pair)
+                                            script)))
+                                 (cadr sources)))
+         (script-path (concat awe:scripts-root-dir script)))
     `(progn
+       ;; define action
        (defun ,action-symbol (candidate)
          (let ((quoted-candidate (format "\"%s\"" candidate)))
            (shell-command-to-string (concat ,script-path " --action=open " quoted-candidate))))
-       (defvar ,source-symbol
-         '((name . ,sname)
-           (init . (lambda ()
-                     (unless (anything-candidate-buffer)
-                       (with-current-buffer
-                           (anything-candidate-buffer 'global)
-                         (message "Building up candidates...")
-                         (insert (shell-command-to-string
-                                  (concat ,script-path " --list")))))))
-           (candidates-in-buffer)
-           (action . ((,aname . ,action-symbol)))))
+       ;; define source
+       ;; XXX
+       ;; ,(message (format "sources length: %d" (length sources)))
+       ;; ,(message (format "first source: %s" (cadr sources)))
+       ;; XXX end
+       (mapcar
+        (lambda (pair)
+          (message (car pair))
+          (defvar ,(intern (format "awe:source-for-%s-in-%s"
+                                   (car pair)
+                                   script))
+            '((name . ,(cdr pair))
+              (init . (lambda ()
+                        (unless (anything-candidate-buffer)
+                          (with-current-buffer
+                              (anything-candidate-buffer 'global)
+                            (message "Building up candidates...")
+                            (insert (shell-command-to-string
+                                     (concat ,script-path " --init=" (car pair))))))))
+              (candidates-in-buffer)
+              (action . ((,aname . ,action-symbol))))))
+        ,'(cadr sources))
+       ;; define anything function
        (defun ,function-symbol ()
          ,description
          (interactive)
-         (anything-other-buffer ,source-symbol
+         (anything-other-buffer '(,source-symbols)
                                 (format "*Anything script: %s" ,script))
          )
-       (defun ,cache-clear-function-symbol ()
-         ,cache-clear-function-description
-         (interactive)
-         (let ((candidate-buffer-name
-                (format " *anything candidates:%s*" ,sname)))
-           (message "Clear cache for anything source: %s" ,sname)
-           (kill-buffer candidate-buffer-name)))
+       ;; (defun ,cache-clear-function-symbol ()
+       ;;   ,cache-clear-function-description
+       ;;   (interactive)
+       ;;   (let ((candidate-buffer-name
+       ;;          (format " *anything candidates:%s*" ,sname)))
+       ;;     (message "Clear cache for anything source: %s" ,sname)
+       ;;     (kill-buffer candidate-buffer-name)))
        (message (concat "Defined functions for " ,script))
        )))
 
-(provide 'anything-with-everything)
+;; Simple
+(add-anything-plugin "anything-hotentry-1" "get-hotentries.pl"
+                     "Description"
+                     '(("list" . "Hot Entries")
+                       ("my_entries" . "My Entries")
+                       ("list" . "Hot Entries")))
+(anything-hotentry-1)
+(anything-hotentry-1-clear-cache)
+
 
 
 ;; (add-anything-plugin "function-name"
 ;;                      "script.pl"
 ;;                      "description"
-;;                      '(("option-value1" . "Source1 Name") ; --action=option-value1が実行される
+;;                      '(("option-value1" . "Source1 Name") ; script --init=option-value1が実行される
 ;;                        ("option-value2" . "Source2 Name")
 ;;                        )
 ;;                      '(("action1-option" . "Action1 Name"))
@@ -87,11 +121,19 @@
 ;; (add-anything-plugin "function-name"
 ;;                      "script.pl"
 ;;                      "description"
-;;                      "Source1 Name"     ; script --listを実行
-;;                      "Action1 Name"     ; script --openを実行
+;;                      "Source1 Name"     ; script --init=listを実行
+;;                      "Action1 Name"     ; script --action=openを実行
 ;;                      )
 
 ;; ;; Simplest
-;; (add-anything-plugin "anything-hotentry-simple" "get-hotentries.pl")
-;; (anything-hotentry-simple)
-;; (anything-hotentry-simple-clear-cache)
+;; (add-anything-plugin "anything-hotentry-1" "get-hotentries.pl")
+;; (anything-hotentry-1)
+;; (anything-hotentry-1-clear-cache)
+
+;; ;; Simplest
+;; (add-anything-plugin "anything-hotentry-1" "get-hotentries.pl")
+;; (anything-hotentry-1)
+;; (anything-hotentry-1-clear-cache)
+
+
+(provide 'anything-with-everything)
